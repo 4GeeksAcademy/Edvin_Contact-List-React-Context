e8e77f3e050e4eeb6fc2c256d6885e3daee37ab6
Edvin Johannes Piirsalu <edvin.piirsalu@gmail.com>
2024-11-16 12:28:23 +0000

    added style and bootstrap

--- src/js/component/ProfileCard.jsx
+++ src/js/component/ProfileCard.jsx
@@ -9,77 +9,37 @@ export const ProfileCard = (props) => {
     const { id, name, address, email, phone } = props.contact
     return (
         <article className="card">
-            <figure className="d-flex">
+            <figure className="card-body d-flex justify-content-around">
                 <img
-                    className="rounded img-fluid"
+                    className="rounded-circle img-fluid"
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5I-LyW9aEufGed1suUqzi6eoGmm0KlsUu1w&s"
                     alt="{name}"
-                    width={'200px'}
-                    height={'200px'}
+                    width={'120px'}
+                    height={'120px'}
+                
                 />
-            </figure>
-            <div className="container">
-                <p className="fs-4">
+            <div className="">
+                <h5 className="card-title mb-1">
                     {name}
+                </h5>
+                <p className="card-text mb-1">
+                <i className="fas fa-map-marker-alt me-2"></i>{address}
                 </p>
-                <p>
-                    {address}
+                <p className="card-text mb-1">
+                <i className="fas fa-envelope me-2"></i>{email}
                 </p>
-                <p>
-                    {email}
+                <p className="card-text mb-1">
+                    <i className="fas fa-phone me-2"></i>{phone}
                 </p>
-                <p>
-                    {phone}
-                </p>
-                <Link onClick={() => actions.selectContact(props.contact)} to={'/contacto/' + id}>
-                    Editar
+                <Link className="btn btn-light btn-sm" onClick={() => actions.selectContact(props.contact)} to={'/contacto/' + id}>
+                <i className="fas fa-pencil-alt"></i>Editar
                 </Link>
-                <button onClick={()=>actions.deleteContact(id)}>
-                    Eliminar
+                <button
+                 className="btn btn-light btn-sm" onClick={()=>actions.deleteContact(id)}>
+                <i className="fas fa-trash"></i>Eliminar
                 </button>
             </div>
+                     </figure>
         </article>
     );
 };
-
-
-// kasutada párast poole=>
-
-
-// <>
-//     <div>
-//         {contacts.map((contact, index) => (
-//             <div key={index} className="card mb-3 shadow-sm">
-//                 <div className="card-body d-flex align-items-center">
-//                     <img
-//                         src={contact.image}
-//                         alt={contact.name}
-//                         className="rounded-circle me-3"
-//                         style={{ width: '60px', height: '60px' }}
-//                         />
-//                     <div className="flex-grow-1">
-//                         <h5 className="card-title mb-1">{contact.name}</h5>
-//                         <p className="card-text mb-1">
-//                             <i className="fas fa-map-marker-alt me-2"></i>{contact.address}
-//                         </p>
-//                         <p className="card-text mb-1">
-//                             <i className="fas fa-phone me-2"></i>{contact.phone}
-//                         </p>
-//                         <p className="card-text mb-1">
-//                             <i className="fas fa-envelope me-2"></i>{contact.email}
-//                         </p>
-//                     </div>
-//                     <div>
-//                         <button className="btn btn-light me-2">
-//                             <i className="fas fa-pencil-alt"></i>
-//                         </button>
-//                         <button className="btn btn-light">
-//                             <i className="fas fa-trash"></i>
-//                         </button>
-//                     </div>
-//                 </div>
-//             </div>
-//         ))}
-//     </div>
-//         </>
-
