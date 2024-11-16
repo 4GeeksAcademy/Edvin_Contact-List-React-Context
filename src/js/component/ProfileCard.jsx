7ef329fec7ed234f303514fd3db642d85c8ab7a6
Edvin Johannes Piirsalu <edvin.piirsalu@gmail.com>
2024-11-16 12:38:21 +0000

    moved delete and edit icon to the right

--- src/js/component/ProfileCard.jsx
+++ src/js/component/ProfileCard.jsx
@@ -16,30 +16,32 @@ export const ProfileCard = (props) => {
                     alt="{name}"
                     width={'120px'}
                     height={'120px'}
-                
+
                 />
-            <div className="">
-                <h5 className="card-title mb-1">
-                    {name}
-                </h5>
-                <p className="card-text mb-1">
-                <i className="fas fa-map-marker-alt me-2"></i>{address}
-                </p>
-                <p className="card-text mb-1">
-                <i className="fas fa-envelope me-2"></i>{email}
-                </p>
-                <p className="card-text mb-1">
-                    <i className="fas fa-phone me-2"></i>{phone}
-                </p>
-                <Link className="btn btn-light btn-sm" onClick={() => actions.selectContact(props.contact)} to={'/contacto/' + id}>
-                <i className="fas fa-pencil-alt"></i>Editar
-                </Link>
-                <button
-                 className="btn btn-light btn-sm" onClick={()=>actions.deleteContact(id)}>
-                <i className="fas fa-trash"></i>Eliminar
-                </button>
-            </div>
-                     </figure>
+                <div className="">
+                    <h5 className="card-title mb-1">
+                        {name}
+                    </h5>
+                    <p className="card-text mb-1">
+                        <i className="fas fa-map-marker-alt me-2"></i>{address}
+                    </p>
+                    <p className="card-text mb-1">
+                        <i className="fas fa-envelope me-2"></i>{email}
+                    </p>
+                    <p className="card-text mb-1">
+                        <i className="fas fa-phone me-2"></i>{phone}
+                    </p>
+                        </div>
+                    <div>
+                        <Link className="btn btn-light btn-sm" onClick={() => actions.selectContact(props.contact)} to={'/contacto/' + id}>
+                            <i className="fas fa-pencil-alt"></i>Editar
+                        </Link>
+                        <button
+                            className="btn btn-light btn-sm" onClick={() => actions.deleteContact(id)}>
+                            <i className="fas fa-trash"></i>Eliminar
+                        </button>
+                    </div>
+            </figure>
         </article>
     );
 };
